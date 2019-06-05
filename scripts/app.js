8c224119e17a984fee969ac4558ab368a1a1475a
LeeX <a543748616@gmail.com>
2019-06-05 19:39:39 +0800

    推荐页面完成

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,18 @@
 $(document).ready(function () {
-    let slider = new Slider(".swiper-container",
-        [{
-                href: "#",
-                img: "images/slide1.jpg"
-            },
-            {
-                href: "#",
-                img: "images/slide2.jpg"
-            },
-            {
-                href: "#",
-                img: "images/slide3.jpg"
-            },
-            {
-                href: "#",
-                img: "images/slide4.jpg"
-            },
-            {
-                href: "#",
-                img: "images/slide5.jpg"
-            }
-        ])
-    slider.render()
+    fetch("json/slide.json")
+        .then((resp) => resp.json())
+        .then(init_slide)
+
+    function init_slide(slide) {
+        let slider = new Slider(".swiper-container",
+            slide.data.slider.map((item) => {
+                return {
+                    href: item.linkUrl,
+                    img: item.picUrl
+                }
+            }))
+        slider.render()
+    }
 
     let nav = new NavigationBar($("nav>ul>li"), $("section>div"))
 })
--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,7 @@
 class NavigationBar {
     constructor($bar_btn, $ctl_view) {
         $($bar_btn[0]).addClass("active")
+        $($ctl_view[0]).siblings().hide()
         $bar_btn.on("click", (e) => {
             $(e.currentTarget)
                 .addClass("active")
